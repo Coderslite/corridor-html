bfb400bc2a9b01a3078baafd214c3516dfae3a08
Coderslite <abrahamgreatebele@gmail.com>
2024-01-16 02:44:12 +0100

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,108 @@
+
+<!--===== FOOTER STARTS=======-->
+<div class="footer4-section-area8">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="contcat5-section5-area section-padding15">
+          <div class="row">
+            <div class="col-lg-3">
+              <div class="contact5-logo5-text1">
+                <img src="assets/images/logo/logo13.png" alt="">
+                <p class="font-ks font-16 weight-500 lineh-26 colorhp1 margin-t margin-b24">Include any additional information that may be relevant or helpful for visitors, such as FAQs, pricing options, or service packages. Provide contact details.</p>
+                <div class="icon-social-links list8">
+                  <ul>
+                    <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                    <li><a href="#" class="ml1"><i class="fa-brands fa-google-plus-g"></i></a></li>
+                    <li><a href="#" class="ml1"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                    <li><a href="#" class="ml1"><i class="fa-brands fa-youtube"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-6">
+              <div class="about-5-links footer8links about40links">
+                <h1 class="font-20 font-lora weight-600 lineh-20 color39">About Link</h1>
+                <ul>
+                  <li><a href="about.html" class="font-ks font-16 lineh-16 weight-500 colorhp1 d-inline-block margin-t32">About Us</a></li>
+                  <li><a href="blog2.html" class="font-ks font-16 lineh-16 weight-500 colorhp1 margin-t d-inline-block">Our Blog</a></li>
+                  <li><a href="servicev1.html" class="font-ks font-16 lineh-16 weight-500 colorhp1 margin-t d-inline-block">Practice Areas</a></li>
+                  <li><a href="casestudy1.html" class="font-ks font-16 lineh-16 weight-500 colorhp1 margin-t d-inline-block">Case Studies</a></li>
+                  <li><a href="testimonials.html" class="font-ks font-16 lineh-16 weight-500 colorhp1 margin-t d-inline-block">Testimonials</a></li>
+                  <li><a href="contact1.html" class="font-ks font-16 lineh-16 weight-500 colorhp1 margin-t d-inline-block">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+              <div class="contact-footer contactfooter8">
+                <h1 class="font-20 color39 lineh-20 font-ks margin-b24 weight-600">Contact Us</h1>
+                <div class="email4">
+                    <img src="assets/images/icons/email5.png" alt="">
+                    <a href="mailto:info@taxvice.com" class="font-16 lineh-24 colorhp1 weight-500 font-ks">info@taxvice.com</a>
+                </div>
+                <span class="location-fooetr ">
+                  <img src="assets/images/icons/location4.svg" alt="" class="heightwidth">
+                <a href="#" class="font-16 font-ks lineh-26 weight-500 colorhp1 margin-t">8708 Technology Forest <br> Pl Suite 125 -G, The <br> Woodlands, TX 77381</a>
+                </span>
+                <div class="email4 margin-t">
+                    <img src="assets/images/icons/phone7.svg" alt="">
+                    <a href="tel:123-456-7890" class="font-16 lineh-24 colorhp1 weight-500 font-ks">123-456-7890</a>
+                </div>
+            </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="contactcta5-form">
+                <div class="contact6-ctafrom8">
+                  <h1 class="font-20 font-lora weight-600 lineh-20 color39">Subscribe Our Newsletter</h1>
+                  <form class="form7">
+                    <input type="text" placeholder="Enter Your email">
+                    <div class="contact5-btn15">
+                      <button class="font-ks font-16 weight-700 lineh-16 color consulitingbt7 text-right">Subscribe <span><i class="fa-solid fa-arrow-right"></i></span></button>
+                     </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img src="assets/images/elementor/elementor54.png" alt="" class="elementors18">
+          <img src="assets/images/elementor/elementor55.png" alt="" class="elementor19">
+        </div>
+        <div class="border7"></div>
+          <div class="copyright6 text-center">
+            <p class="font-ks lineh-26 weight-500 font-16 colorhp1">© Copyright 2023 Consalt Law Consultant, Designed by Fleexstudio</p>
+          </div>
+      </div>
+    </div>
+  </div>
+ 
+</div>
+<!--===== FOOTER END=======-->
+
+  <!--=====JS=======-->
+  <script src="assets/js/plugins/bootstrap.min.js"></script>
+  <script src="assets/js/plugins/swiper.bundle.js"></script>
+  <script src="assets/js/plugins/owl.carousel.min.js"></script>
+  <script src="assets/js/plugins/jquery-modal-video.min.js"></script>
+  <script src="assets/js/plugins/fontawesome.js"></script>
+  <script src="assets/js/plugins/counter.js"></script>
+  <script src="assets/unpkg.com/aos%402.3.1/dist/aos.js"></script>
+  <script src="assets/js/plugins/jquery.nice-select.js"></script>
+  <script src="assets/cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>
+  <script src="assets/js/plugins/jquery.lineProgressbar.js"></script>
+  <script src="assets/js/plugins/isotope.pkgd.min.js"></script>
+  <script src="assets/js/plugins/propper.min.js"></script>
+  <script src="assets/unpkg.com/imagesloaded%405.0.0/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/5.0.0/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/js/plugins/tippy.js"></script>
+  <script src="assets/js/plugins/jquery.nice-select.js"></script>
+  <script src="assets/cdn.jsdelivr.net/npm/%40popperjs/core%402.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="assets/js/plugins/mobilemenu.js"></script>
+   <script src="assets/js/main.js"></script>
+    
+
+</body>
+
+<!-- Mirrored from html.fleexstudio.com/consult/index8.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Jan 2024 01:02:35 GMT -->
+</html>
